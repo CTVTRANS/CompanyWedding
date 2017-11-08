72015970a95544cf6d20fb0d2ba7c5270cd2b172
le van kien <kien@les-iMac.local>
2017-11-08 15:20:49 +0700

    Initial Commit

---
+++ CompanyWedding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CompanyWedding
+//
+//  Created by le kien on 11/8/17.
+//  Copyright © 2017 le kien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
